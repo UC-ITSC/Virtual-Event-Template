024b9291d3f60d608fbe0d67b17cd235f322ea00
Wesley Reed <reedws@ucmail.uc.edu>
2020-08-28 13:27:30 -0400

    oops

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { addPrefetchExcludes, Root, Routes } from 'react-static';
 import { Link, Router } from '@reach/router';
 import FancyDiv from 'components/FancyDiv';
 import Dynamic from 'containers/Dynamic';
-import './app.css';
+import './app.scss';
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes([ `dynamic` ]);
