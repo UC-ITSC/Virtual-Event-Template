3321ce88984ba4833821fa8bb58ebc73c9e14366
Wesley Reed <reedws@ucmail.uc.edu>
2020-11-09 11:07:54 -0500

    layout components to FC

--- src/components/layout-components/Header/index.js
+++ src/components/layout-components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
 import clsx from 'clsx';
 import { Link } from '@reach/router';
@@ -6,51 +6,44 @@ import { Nav, NavbarBrand, NavItem, NavLink } from 'reactstrap';
 
 import projectLogo from '../../../assets/images/react.svg';
 
-class Header extends Component {
-  render() {
-    const { headerShadow } = this.props;
-
-    return (
-      <Fragment>
-        <div className="header-nav-wrapper header-nav-wrapper-lg" />
-        <div
-          className={clsx(
-            `app-header bg-second header-nav-wrapper header-nav-wrapper-lg w-100 navbar-light`,
-            { 'app-header--shadow': headerShadow },
-          )}>
-          <div className="container-fluid">
-            <Nav pills className="mr-auto align-items-center">
-              <NavbarBrand to="/" title="Bamburgh React UI Kit with Reactstrap Free">
-                <i className="bg-transparent">
-                  <img
-                    alt="Bamburgh React UI Kit with Reactstrap Free"
-                    style={{ width: `50px` }}
-                    src={projectLogo}
-                  />
-                </i>
-                <span className="text-white pl-2">Bamburgh</span>
-              </NavbarBrand>
-              <NavItem>
-                <NavLink tag={Link} className="d-block mr-1" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="d-block mr-1" to="/blog">Blog</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="d-block mr-1" to="/example-page">Template Page</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="d-block mr-1" to="#/">Link</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} disabled className="d-block mr-1" to="#/">Disabled</NavLink>
-              </NavItem>
-            </Nav>
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
-}
+const Header = ({ headerShadow }) =>
+  <Fragment>
+    <div className="header-nav-wrapper header-nav-wrapper-lg" />
+    <div
+      className={clsx(
+        `app-header bg-second header-nav-wrapper header-nav-wrapper-lg w-100 navbar-light`,
+        { 'app-header--shadow': headerShadow },
+      )}>
+      <div className="container-fluid">
+        <Nav pills className="mr-auto align-items-center">
+          <NavbarBrand to="/" title="Bamburgh React UI Kit with Reactstrap Free">
+            <i className="bg-transparent">
+              <img
+                alt="Bamburgh React UI Kit with Reactstrap Free"
+                style={{ width: `50px` }}
+                src={projectLogo}
+              />
+            </i>
+            <span className="text-white pl-2">Bamburgh</span>
+          </NavbarBrand>
+          <NavItem>
+            <NavLink tag={Link} className="d-block mr-1" to="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} className="d-block mr-1" to="/blog">Blog</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} className="d-block mr-1" to="/example-page">Template Page</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} className="d-block mr-1" to="#/">Link</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} disabled className="d-block mr-1" to="#/">Disabled</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    </div>
+  </Fragment>;
 
 export default Header;
--- src/components/layout-components/PageTitle/index.js
+++ src/components/layout-components/PageTitle/index.js
@@ -1,21 +1,15 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
-class PageTitle extends Component {
-  render() {
-    const { titleDescription, titleHeading } = this.props;
-    return (
-      <Fragment>
-        <div className="mb-5">
-          <h1 className="display-3 mb-3 font-weight-bold text-capitalize">
-            {titleHeading}
-          </h1>
-          <p className="font-size-xl text-black-50">
-            {titleDescription}
-          </p>
-        </div>
-      </Fragment>
-    );
-  }
-}
+const PageTitle = ({ titleDescription, titleHeading }) =>
+  <Fragment>
+    <div className="mb-5">
+      <h1 className="display-3 mb-3 font-weight-bold text-capitalize">
+        {titleHeading}
+      </h1>
+      <p className="font-size-xl text-black-50">
+        {titleDescription}
+      </p>
+    </div>
+  </Fragment>;
 
 export default PageTitle;
