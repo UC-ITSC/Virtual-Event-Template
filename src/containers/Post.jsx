1575dcd23ef0dd739f2b8ef737be34d398b6c185
Wesley Reed <reedws@ucmail.uc.edu>
2020-11-09 11:08:32 -0500

    add default content class

--- src/containers/Post.jsx
+++ src/containers/Post.jsx
@@ -4,12 +4,15 @@ import { Link } from '@reach/router';
 
 const PostComponent = () => {
   const { post } = useRouteData();
+
   return (
-    <div>
-      <Link to="/blog/">{`<`} Back</Link>
-      <br />
-      <h3>{post.title}</h3>
-      <p>{post.body}</p>
+    <div className="default-content">
+      <div className="content">
+        <Link to="/blog/">{`<`} Back</Link>
+        <br />
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
     </div>
   );
 };
